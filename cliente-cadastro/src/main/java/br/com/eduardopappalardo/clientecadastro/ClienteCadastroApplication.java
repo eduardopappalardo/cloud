95680ff7c5ff7f6178cc8b0a5042b32d2f5d8632
Eduardo <eduardopappalardo@yahoo.com.br>
2018-08-15 00:51:42 -0300

    :)

---
+++ cliente-cadastro/src/main/java/br/com/eduardopappalardo/clientecadastro/ClienteCadastroApplication.java
@@ -0,0 +1,12 @@
+package br.com.eduardopappalardo.clientecadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteCadastroApplication.class, args);
+	}
+}
